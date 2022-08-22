cdde5abd91954a9356711c42e1a14752a3d1ccf2
Netfix <a.v.dobroskok.kh@gmail.com>
2022-08-22 20:18:04 +0300

    Create a tiny world

--- index.js
+++ index.js
@@ -1,87 +1,75 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
+   Code repository: https://github.com/OlexiyDobroskok/a-tiny-JS-world
    Web app: _put project's github pages URL here_
    */
 
 // ======== OBJECTS DEFINITIONS ========
 
-const aTinyWorld = [
-  {
-    man: {
-      species: "human",
-      name: "José",
-      gender: "male",
-      legs: 2,
-      hands: 2,
-      saying: "Hola, amigo!",
-      friends: ["Nerea", "Lalo"],
-    },
-  },
-  {
-    woman: {
-      species: "human",
-      name: "Martina",
-      gender: "female",
-      legs: 2,
-      hands: 2,
-      saying: "Buenos días!",
-      friends: ["Pako"],
-    },
-  },
-  {
-    cat: {
-      species: "cat",
-      name: "Lalo",
-      gender: "female",
-      legs: 4,
-      hands: 0,
-      saying: "¡miau miau!",
-      friends: ["José", "Nerea"],
-    },
-  },
-  {
-    dog: {
-      species: "dog",
-      name: "Pako",
-      gender: "male",
-      legs: 4,
-      hands: 0,
-      saying: "¡guau guau!",
-      friends: ["Martina"],
-    },
-  },
-  {
-    сatwoman: {
-      species: "human",
-      name: "Nerea",
-      gender: "female",
-      legs: 2,
-      hands: 2,
-      saying: " muchachos!",
-      friends: ["José", "Lalo"],
-    },
-  },
-];
+const man = {
+  species: "human",
+  name: "José",
+  gender: "male",
+  legs: 2,
+  hands: 2,
+  saying: "Hola, amigo!",
+  friends: ["Nerea", "Lalo"],
+};
+
+const woman = {
+  species: "human",
+  name: "Martina",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: "Buenos días!",
+  friends: ["Pako"],
+};
+
+const cat = {
+  species: "cat",
+  name: "Lalo",
+  gender: "female",
+  legs: 4,
+  hands: 0,
+  saying: "¡miau miau!",
+  friends: ["José", "Nerea"],
+};
+
+const dog = {
+  species: "dog",
+  name: "Pako",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "¡guau guau!",
+  friends: ["Martina"],
+};
+
+const catwoman = {
+  species: "human",
+  name: "Nerea",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: cat.saying + " muchachos!",
+  friends: ["José", "Lalo"],
+};
 
 // ======== OUTPUT ========
-aTinyWorld.forEach((el) => print(`${obj.prop1}; ${obj.prop2}; ${obj.prop3};`));
 
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+const aTinyWorld = [man, woman, cat, dog, catwoman];
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const residentsProperties = aTinyWorld.map((resident) => {
+  const allProperties = [];
+  for (let prop in resident) {
+    if (resident[prop] === 0) {
+      continue;
+    }
+    allProperties.push(resident[prop]);
+  }
+  return allProperties;
+});
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+residentsProperties.forEach((properties) => print(properties.join("; ")));
